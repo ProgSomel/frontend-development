d50d7f57c43c9cb5df747a3ba80bdafa4a375fa7
ProgSomel <somelahmed55@gmail.com>
2025-07-01 11:33:27 +0600

    primitive and reference added

--- JavaScript/js/script.js
+++ JavaScript/js/script.js
@@ -1,6 +1,9 @@
-//? Select button
-const button = document.getElementById("btn");
-button.addEventListener("click", hello)
-function hello(){
-    console.log("Hello World");
-}
+let a = ["JS", "Python"];
+let b = ["HTML", "CSS"];
+
+b = a;
+console.log(a);
+console.log(b);
+a.push("Go");
+console.log(a);
+console.log(b);
