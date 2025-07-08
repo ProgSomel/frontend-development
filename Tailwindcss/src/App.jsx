4f5ea23767c3bd8e94403d09fd6a525871c0b535
ProgSomel <somelahmed55@gmail.com>
2025-07-08 18:16:56 +0600

    variants added

--- Tailwindcss/src/App.jsx
+++ Tailwindcss/src/App.jsx
@@ -2,31 +2,8 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900'>
-     <div className='max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md space-y-2
-     sm:flex sm:items-center sm:py-4 sm:space-y-0 sm:space-x-12
-     dark:bg-gray-800 sm:dark:hover:bg-gray-700
-     '>
-      <img className='h-24 mx-auto rounded-full ring-4 
-      ring-green-400 sm:mx-0 sm:shrink-0
-      transform hover:scale-105 duration-500
-      ' src="../images/logo.png" alt="" />
-      <div className='text-center space-y-2'>
-        <div className='space-y-0.5'>
-          <h1 className='
-          text-black font-semibold
-          dark:text-gray-300
-          '>Learn with Sumit</h1>
-          <p 
-          className='
-          text-gray-500 font-medium
-          dark:text-gray-400 text-body
-          '>Youtube Channel</p>
-        </div>
-        <button className='btn btn-purple rounded-sm'>Visit Now</button>
-        <button className='btn btn-green'>Website</button>
-      </div>
-     </div>
+    <div className='hover:rotate-0'>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque possimus illo veritatis, voluptate id in expedita sapiente magni, aliquam quasi sed. Obcaecati, et? Culpa officiis, dicta neque omnis illum hic eveniet soluta voluptatum, cum explicabo velit, voluptates quos unde similique.
     </div>
   )
 }
